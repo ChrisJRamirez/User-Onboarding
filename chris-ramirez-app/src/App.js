302a61a58657945385146c06a0a13e894454e703
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-07-13 17:54:41 -0400

    adding in more dependicies and working through project

--- chris-ramirez-app/src/App.js
+++ chris-ramirez-app/src/App.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 import axios from "axios";
 import * as yup from "yup"
+import Form from "./Form"
+import schema from "./Schema"
+import {validate} from "uuid"
 
 const initialFormValues = {
   //text inputs
@@ -28,7 +31,52 @@ function App() {
   const [users, setUsers] = useState(initialUsers)
   const [formValues, setFormValues] = useState(initialFormValues)
   const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
 
+  //helper functions
+  const getUsers = () =>{
+    axios.get("https://reqres.in/api/users")
+      .then(res => {
+        setUsers(res.data)   // may have to double check res.data if not working later *****
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const postNewUser = newUser => {
+    axios.post("https://reqres.in/api/users", newUser)
+      .then(res => {
+        setUsers([...users, res.data])
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors, [name]: ""
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors, [name]: err.errors[0]
+        })
+      })
+  }
+
+
+  //event handler
+  
   
   
   
